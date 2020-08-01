180fad4c5e3961a5de7d4b3f843865333d27a957
Mr. Phantom <phantom@phantom.im>
2020-08-01 14:01:47 -0500

    Update myscript.py

--- myscript.py
+++ myscript.py
@@ -4,3 +4,4 @@ random_number = random.randint(0, 1000000)
 
 with open(f"./{random_number}.txt", "w") as temp_file:
     temp_file.write(f"Hey, {random_number}")
+    print("Created file:", random_number)
