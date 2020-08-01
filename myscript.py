bb0d981f73ec1b1851c65ac5907c94ad61ad3077
Mr. Phantom <phantom@phantom.im>
2020-08-01 15:32:33 -0500

    Update myscript.py

--- myscript.py
+++ myscript.py
@@ -1,7 +1,10 @@
+import os
 import random
+
+
+os.makedirs("./data", exist_ok=True)
 
 random_number = random.randint(0, 1000000)
 
-with open(f"./{random_number}.txt", "w") as temp_file:
+with open(f"./data/{random_number}.txt", "w") as temp_file:
     temp_file.write(f"Hey, {random_number}")
-    print("Created file:", random_number)
